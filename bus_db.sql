e3f8137b55defd38424ea8d1bb19680c7ebb42af
div-yam <f20191072@goa.bits-pilani.ac.in>
2024-01-06 01:44:33 +0530

    Repo Setup

---
+++ bus_db.sql
@@ -0,0 +1,10 @@
+drop database busdb;
+drop user busmanager;
+create user busmanager with password 'password'
+create database busdb with template=template0 owner=busmanager;
+\connect busdb;
+alter default privileges grant all on tables to busmanager;
+
+create table bus(
+id varchar(20) not null
+);
---
+++ src/test/java/com/busManager/busmanager/BusManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busManager.busmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
